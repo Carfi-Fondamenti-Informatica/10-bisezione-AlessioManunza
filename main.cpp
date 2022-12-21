ba213ab645f9f24d9f8c9e108bbf14a457a5b9f2
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-21 10:33:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float funzione (float y) {
+    y=pow (y , 2)*cos(y)+1 ;
+    return y ;
+}
+
+float bisezione (float a , float b) {
+    int i = 1;
+    float x = 0 ,z=0 ;
+    float err = 0;
+    do {
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+    } while ((funzione(a) * funzione(b)) >= 0);
+    while (i>0) {
+        x=(a+b)/2 ;
+        if (funzione(x)==0){
+            cout << x << endl;
+            cout << funzione(x) << endl;
+        }
+        else  {
+            z=funzione(a)*funzione(b) ;
+        }
+        while (i>0) {
+            if (z < 0) {
+                b = x;
+            } else {
+                a = x;
+            }
+            err=abs(((b-6)/2));
+            if (err>=(1/pow(M_E , -6))){
+                break;
+            }
+            else
+            {
+                cout << x << endl;
+                cout << funzione(x) << endl;
+                i-- ;
+            }
+        }
+    }
+}
+
 int main() {
+   float a , b ;
+    bisezione (a , b) ;
    return 0;
 }
